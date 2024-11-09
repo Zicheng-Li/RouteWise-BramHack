0f64f865e3305e08a79f183eee0f229c3bc28988
self-sasi <sarthaks281bond@gmail.com>
2024-11-08 21:12:12 -0500

    theme setting and folder formation in app folder

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppConfig, LayoutService } from './layout/service/app.layout.service';
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 
@@ -6,9 +7,21 @@ import { PrimeNGConfig } from 'primeng/api';
     templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-    constructor(private primengConfig: PrimeNGConfig) {}
+    constructor(private primengConfig: PrimeNGConfig, private layoutService: LayoutService) {}
 
     ngOnInit(): void {
         this.primengConfig.ripple = true;
+
+        // theme configuration (slim color filled sidebar, light mode, green primary color)
+        const config: AppConfig = {
+            ripple: false,
+            inputStyle: 'outlined',
+            menuMode: 'slim',
+            colorScheme: 'light',
+            theme: 'green',
+            menuTheme: "primaryColor",
+            scale: 14
+        };
+        this.layoutService.config.set(config);
     }
 }

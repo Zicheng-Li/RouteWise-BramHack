26c2856c7720a3893a04f17d5c4281f214f4a9c1
ZL&EL <lzc123456ab@gmail.com>
2024-11-09 15:09:39 -0500

    set up gpt connection

--- frontend/src/app/subpages/chats/chats.component.ts
+++ frontend/src/app/subpages/chats/chats.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { AiService } from 'src/app/services/ai/ai.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-chats',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './chats.component.html',
   styleUrl: './chats.component.scss'
 })
 export class ChatsComponent {
+  userInput:string='';
+  response:string | null=null;
+
+  constructor(private gptService: AiService){}
+
+  getResponse() {
+    if(this.userInput.trim()){
+      this.gptService.generateText(this.userInput)
+      .subscribe(response => {
+        this.response=response.choices[0].message.content; 
+        this.userInput='';
+        console.log(response);
+      });
+    }
+    
+    }
 
 }

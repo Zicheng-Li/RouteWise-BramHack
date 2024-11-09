0b0dbbdf2b4184d3fe3fc1de5986b397d69ffa16
self-sasi <sarthaks281bond@gmail.com>
2024-11-08 22:51:32 -0500

    dashboard creation with placeholders

--- frontend/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ frontend/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,12 +1,179 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ChartModule } from 'primeng/chart';
+import { RippleModule } from 'primeng/ripple';
+import { TagModule } from 'primeng/tag';
+import { TooltipModule } from 'primeng/tooltip';
+import { Subscription, debounceTime } from 'rxjs';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
+
+interface MonthlyPayment {
+    name?: string;
+    amount?: number;
+    paid?: boolean;
+    date?: string;
+}
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    ButtonModule,
+    RippleModule,
+    TagModule,
+    TooltipModule,
+    TableModule,
+    InputNumberModule,
+    ChartModule],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
-export class DashboardPageComponent {
 
+export class DashboardPageComponent implements OnInit, OnDestroy {
+    chartData: any;
+
+    chartOptions: any;
+
+    payments: MonthlyPayment[] = [];
+
+    subscription: Subscription;
+
+    constructor(private layoutService: LayoutService) {
+        this.subscription = this.layoutService.configUpdate$
+            .pipe(debounceTime(25))
+            .subscribe((config) => {
+                this.initChart();
+            });
+    }
+
+    ngOnInit() {
+        this.initChart();
+
+        this.payments = [
+            {
+                name: 'Electric Bill',
+                amount: 75.6,
+                paid: true,
+                date: '06/04/2022',
+            },
+            {
+                name: 'Water Bill',
+                amount: 45.5,
+                paid: true,
+                date: '07/04/2022',
+            },
+            { name: 'Gas Bill', amount: 45.2, paid: false, date: '12/04/2022' },
+            {
+                name: 'Internet Bill',
+                amount: 25.9,
+                paid: true,
+                date: '17/04/2022',
+            },
+            {
+                name: 'Streaming',
+                amount: 40.9,
+                paid: false,
+                date: '20/04/2022',
+            },
+        ];
+    }
+
+    initChart() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue(
+            '--text-color-secondary'
+        );
+        const surfaceBorder =
+            documentStyle.getPropertyValue('--surface-border');
+
+        this.chartData = {
+            labels: [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+            ],
+            datasets: [
+                {
+                    label: 'Target',
+                    data: [6500, 5900, 8000, 8100, 5600, 5500, 4000],
+                    fill: false,
+                    tension: 0.4,
+                    borderColor: documentStyle.getPropertyValue('--green-500'),
+                },
+                {
+                    label: 'Personal Expenses',
+                    data: [1200, 5100, 6200, 3300, 2100, 6200, 4500],
+                    fill: true,
+                    borderColor: '#6366f1',
+                    tension: 0.4,
+                    backgroundColor: 'rgba(99,102,220,0.2)',
+                },
+            ],
+        };
+
+        this.chartOptions = {
+            animation: {
+                duration: 0,
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor,
+                    },
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function (context: any) {
+                            let label = context.dataset.label || '';
+
+                            if (label) {
+                                label += ': ';
+                            }
+
+                            if (context.parsed.y !== null) {
+                                label += new Intl.NumberFormat('en-US', {
+                                    style: 'currency',
+                                    currency: 'USD',
+                                }).format(context.parsed.y);
+                            }
+                            return label;
+                        },
+                    },
+                },
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                    },
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary,
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                    },
+                },
+            },
+        };
+    }
+
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
 }
